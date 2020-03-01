970df87210d411f307d943ae3c56506dccfa2e8d
unknown <dng360@ucmerced.edu>
2020-02-29 23:55:16 -0800

    changes

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ CC = g++
 
 all:
 	$(CC) -std=c++11 -o a.exe main.cpp
-
+	CFLAGS = -Iinc -Idep -w -std=c++11 -lboost_system -lboost_filesystem -lpthread
+	_DEPS = main.h
 clean:
 	rm -f a.exe
 
--- static/main.js
+++ static/main.js
@@ -1,6 +1,66 @@
+var name   = document.getElementById("myName").value;
+var price   = document.getElementById("myPrice").value;
+var quantity   = document.getElementById("myQuantity").value;
+var array = new Array(5);
+for(var i = 0; i < array.length; i++){
+    array[i] = [name, price, quantity, category];
+}
+function calculate(){
+    var totalPrice, totalQuantity, i;
+    largestPrice = 0;
+    largestQuantity = 0;
+    var percentPrice = new Array(5);
+    var percentQuantity = new Array(5);
+    for (i = 0; i < array.length; i++){
+        totalPrice+= array[i][1];
+    }
+    for (i = 0; i < percent.length; i++){
+        totalQuantity+= array[i][2];
+    }
+    console.log(array[1][1] + "" + totalPrice)
+    for (i = 0; i < percentPrice.length; i++){
+        percentPrice[i] = 100 * (array[i][1] / totalPrice); 
+        percentQuantity[i] = 100 * (array[i][2] / totalQuantity);
+    }
+    for (i = 0; i < array.length; i++){
+        if(i != 0){
+        if(percentPrice[i] > largestPrice)
+            largestPrice = percentPrice[i];
+        if(percentQuantity[i] > largestQuantity)
+            largestQuantity = percentQuantity[i];
+        }
+    }
+    for(i = 0; i < array.length; i++){
+    if (percentPrice[i] == largestPrice)
+    console.log("The most expensive item is " + array[i][0] + "taking up" + largestPrice + " percent of the total amount.");
+    if (percentPrice >= 50)
+    console.log("");
+    }
+}
+
 function clickAddRow(){
     $.get("/addRow"),{},function(response){
-        $("/assets/pfpurple.png").html(display);
+       //Implement images/design last
+       //1
+        var table = document.getElementById("myTable");
+        var x  = table.insertRow(0);
+        var a = table.rows.length-1;
+        var l = table.rows[a].cells.length;
+
+        for(var c = 0, m = 1; c < m; c++)
+        {
+            table.rows[0].insertCell(c);
+            table.rows[0].cells[c].innerHTML = "&nbsp;&nbsp";
+
+        }
+       //2
+        $(table).find('tbody').append("<tr><td>aaaa</td></tr>");
+       //3
+        tableRef.insertRow(tableRef.rows.length);
+       //4
+       var tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+
+       var newRow = tableRef.insertRow();
     }
 }
 
